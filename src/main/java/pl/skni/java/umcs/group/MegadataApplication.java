fccf2f03e261b6e8b534038cf489a5175ec8abb1
Jakub Pyda <pyda.jakub@gmail.com>
2016-03-26 17:48:03 +0100

    struktura projektu

---
+++ src/main/java/pl/skni/java/umcs/group/MegadataApplication.java
@@ -0,0 +1,12 @@
+package pl.skni.java.umcs.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegadataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegadataApplication.class, args);
+	}
+}
