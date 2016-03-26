973fbb5e82756f1f6dbfc5e3c54de4699dd7cd65
Jakub Pyda <pyda.jakub@gmail.com>
2016-03-26 18:11:14 +0100

    kontekst testow integracyjnych

--- src/main/java/pl/skni/java/umcs/group/configuration/MegadataApplication.java
+++ src/main/java/pl/skni/java/umcs/group/configuration/MegadataApplication.java
@@ -1,4 +1,4 @@
-package pl.skni.java.umcs.group;
+package pl.skni.java.umcs.group.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MegadataApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MegadataApplication.class, args);
+		SpringApplication.run("META-INF/spring/springConfig.xml", args);
 	}
 }
