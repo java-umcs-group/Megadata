f4c8d3297e13b3d748a83e3a26a43fc147d0ffd8
Jakub Pyda <pyda.jakub@gmail.com>
2016-03-31 18:36:54 +0200

    User class

---
+++ src/main/java/pl/skni/java/umcs/group/model/User.java
@@ -0,0 +1,33 @@
+package pl.skni.java.umcs.group.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by Jakub Pyda on 31.03.2016.
+ */
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue
+    Integer Id;
+
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+}
